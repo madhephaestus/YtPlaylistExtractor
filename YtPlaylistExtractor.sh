0262b3906dd50f35328225e0e55e400d6010c029
Kevin Harrington <mad.hephaestus@gmail.com>
2023-08-03 11:26:02 -0400

    Add ffmpeg test

--- YtPlaylistExtractor.sh
+++ YtPlaylistExtractor.sh
@@ -6,8 +6,9 @@ if test -f "$FILE"; then
 else
 	wget https://github.com/ytdl-patched/yt-dlp/releases/download/2023.02.17.334/yt-dlp_linux
 	chmod +x $FILE
+fi
+if ! [ -x "$(command -v ffmpeg)" ]; then
 	sudo apt install ffmpeg
 fi
-
 
 $FILE --extract-audio --audio-format mp3 --audio-quality 0 -o $2"/%(playlist)s/%(title)s.%(ext)s" $1

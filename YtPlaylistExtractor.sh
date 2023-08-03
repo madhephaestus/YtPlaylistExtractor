49a8c35b22e235ef104450d5da2cb94174c7fb9d
Kevin Harrington <mad.hephaestus@gmail.com>
2023-08-03 10:57:56 -0400

    Adding file and auto installer

--- YtPlaylistExtractor.sh
+++ YtPlaylistExtractor.sh
@@ -1,1 +1,13 @@
-echo Hello World
+#!/bin/bash
+
+FILE=./yt-dlp_linux
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+	wget https://github.com/ytdl-patched/yt-dlp/releases/download/2023.02.17.334/yt-dlp_linux
+	chmod +x $FILE
+	sudo apt install ffmpeg
+fi
+
+
+$FILE --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $1
